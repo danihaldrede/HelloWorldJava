fc8e902a38583064116eae225cf5aad2a23c1503
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-02-26 11:58:40 -0500

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+// Hello World Program 
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.printIn("Hello!";
+  } 
+}
+ 
+ public class HelloWorld {
+  public static void main(String[] args) { 
+  }
+}
